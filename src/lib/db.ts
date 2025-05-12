0b44248051a19b8a8445d4941bfd1082a3aef6fe
Michi Saito <ms20054869tv@gmail.com>
2025-05-12 22:31:11 +0900

    Supabase SDK 削除＆pg接続構成に統一

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,10 @@
-import { Pool } from 'pg';
+import { Pool } from 'pg'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
 
-export default pool;
+export default pool
