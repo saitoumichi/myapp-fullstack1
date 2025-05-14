1ab6ce33cd3cf24fe145956568188a195114f5db
Michi Saito <ms20054869tv@gmail.com>
2025-05-14 18:52:38 +0900

    コード確認依頼

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,7 @@ import { Pool } from 'pg'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
 })
 
 export default pool
